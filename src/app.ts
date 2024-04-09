47d49547b6cca2d57d2faa8fcbb26e3acfa671c0
Russell Yeo <rusyeo@gmail.com>
2024-04-09 08:56:39 +0100

    Add OAuth with temporary file store

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { App, LogLevel } from '@slack/bolt';
+import { FileInstallationStore } from '@slack/oauth';
 import { config } from 'dotenv';
 
 import { APIService } from './infrastructure/api_service';
@@ -6,19 +7,18 @@ import { MentionsController } from './interfaces/mentions_controller';
 import { SlackAdapter } from './infrastructure/slack_adapter';
 
 /** Configure Environment Variables */
+
 config();
 
+const baseURL = process.env.ROTA_API_URL;
+if (!baseURL) {
+  throw new Error('ROTA_API_URL is not defined');
+}
+
 /** Initialization */
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: false,
-  logLevel: LogLevel.DEBUG,
-});
 
 const apiService = new APIService({
-  baseURL: process.env.ROTA_API_URL || '',
+  baseURL: baseURL
 });
 
 const slackAdapter = new SlackAdapter();
@@ -28,13 +28,28 @@ const mentionsController = new MentionsController({
   slackAdapter: slackAdapter
 });
 
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'my-state-secret',
+  scopes: ['app_mentions:read', 'chat:write'],
+  logLevel: LogLevel.DEBUG,
+  installationStore: new FileInstallationStore(),
+  installerOptions: {
+    directInstall: true
+  }
+});
+
 /** Register event handlers */
+
 app.event('app_mention', async ({ event, say }) => {
   slackAdapter.setSayFn(say);
   mentionsController.handleMention(event.text);
 });
 
 /** Start Bolt App */
+
 (async () => {
   try {
     await app.start(Number(process.env.PORT || 3000));
